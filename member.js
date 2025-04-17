8f5ed6347c7da44ec32b4b65d30c7a0662a51801
longye-oshima <tatsuya.ooshima@gmail.com>
2025-04-17 11:38:12 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,16 @@
+function skillsMember() {
+  return (
+    <div className="skills">
+      <div className="skills__member">
+        <h2>Skills</h2>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>React</li>
+          <li>Node.js</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
